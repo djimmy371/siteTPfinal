bba7a974f262ea2650ecdaa2b000e022b168477c
erwangrostoutou <noahdenis85@yahoo.com>
2024-03-21 11:12:54 +0100

    carousel boutton

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,3 @@
-document.querySelector('.carousel-button.next').addEventListener('click', function() {
-  const current = document.querySelector('.carousel-item.active');
-  let next = current.nextElementSibling;
-  if (!next) {
-    // Si on est au dernier élément, revient au début
-    next = document.querySelector('.carousel-item:first-child');
-  }
-  current.classList.remove('active');
-  next.classList.add('active');
-});
-
-document.querySelector('.carousel-button.prev').addEventListener('click', function() {
-  const current = document.querySelector('.carousel-item.active');
-  let prev = current.previousElementSibling;
-  if (!prev) {
-    // Si on est au premier élément, va au dernier
-    prev = document.querySelector('.carousel-item:last-child');
-  }
-  current.classList.remove('active');
-  prev.classList.add('active');
+$('input').on('change', function() {
+  $('body').toggleClass('blue');
 });
