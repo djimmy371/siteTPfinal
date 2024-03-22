72df88d9bf6f8b49821200b2d163dabc0494ffee
djimmy371 <djimmy.allard@hotmail.fr>
2024-03-22 14:08:34 +0100

    soucis trié fix

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
 function openPopup() {
-    var popup = document.getElementById("loginPopup");
-    popup.style.display = "block";
-  }
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
 
-  function closePopup() {
-    var popup = document.getElementById("loginPopup");
-    popup.style.display = "none";
+function closePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("password");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
   }
-    // You can add other scripts here if needed
-  });
+}
