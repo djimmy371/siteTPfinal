efc9764b3e7b8c49daad8f66d164387d487ce90e
djimmy371 <djimmy.allard@hotmail.fr>
2024-03-22 14:05:54 +0100

    Add PopUp

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const items = document.querySelectorAll('.carousel-item');
-    const nextButton = document.querySelector('.next');
-    const prevButton = document.querySelector('.prev');
+document.addEventListener("DOMContentLoaded", function() {
+function openPopup() {
+    var popup = document.getElementById("loginPopup");
+    popup.style.display = "block";
+  }
 
-    let currentIndex = 0;
-
-    function navigate(direction) {
-        items[currentIndex].classList.remove('active');
-        currentIndex += direction;
-        if (currentIndex < 0) { currentIndex = items.length - 1; }
-        if (currentIndex >= items.length) { currentIndex = 0; }
-        items[currentIndex].classList.add('active');
-    }
-
-    nextButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        navigate(1);
-    });
-
-    prevButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        navigate(-1);
-    });
-});
+  function closePopup() {
+    var popup = document.getElementById("loginPopup");
+    popup.style.display = "none";
+  }
+    // You can add other scripts here if needed
+  });
